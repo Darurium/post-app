a5948be265ac82354eef337f7eb3b70b5678236c
Alex <darurium@gmail.com>
2021-04-28 08:06:23 +0500

    first

---
+++ src/componens/app/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+
+import './app.css';
+
+const App = () => {
+    return (
+        <div className='app'>  
+            <AppHeader/>
+            <div className='search-panel d-flex'>
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+        </div>
+    )
+}
+
+export default App;
